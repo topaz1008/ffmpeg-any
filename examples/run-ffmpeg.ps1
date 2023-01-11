64654ebe600e22402e172d2c9133ab83a858f943
Topaz Bar <topaz1008@gmail.com>
2023-01-11 12:47:26 +0200

    (chore): fix ffmpeg command in examples

--- examples/run-ffmpeg.ps1
+++ examples/run-ffmpeg.ps1
@@ -9,13 +9,13 @@ function Invoke-Call {
 }
 
 Invoke-Call -ScriptBlock {
-    ffmpeg -hide_banner -i "video1.mkv" -c:v x264 -c:a aac 128k "video1.mp4"
+    ffmpeg -hide_banner -i "video1.mkv" -c:v libx264 -c:a aac 128k "video1.mp4"
 } -ErrorAction Stop
 Invoke-Call -ScriptBlock {
-    ffmpeg -hide_banner -i "video2.mkv" -c:v x264 -c:a aac 128k "video2.mp4"
+    ffmpeg -hide_banner -i "video2.mkv" -c:v libx264 -c:a aac 128k "video2.mp4"
 } -ErrorAction Stop
 Invoke-Call -ScriptBlock {
-    ffmpeg -hide_banner -i "video3.mkv" -c:v x264 -c:a aac 128k "video3.mp4"
+    ffmpeg -hide_banner -i "video3.mkv" -c:v libx264 -c:a aac 128k "video3.mp4"
 } -ErrorAction Stop
 Invoke-Call -ScriptBlock {
     Remove-Item -Force "run-ffmpeg.ps1"
